4d5651ad4f69b2ae11a10abf2fc72beadbf391e8
Service Web <sw@gssmail.com>
2024-03-12 08:59:08 -0500

    Commit changes in C:\Websites\Git\emarlow\4447\GCG_4447_BomLink_Dir.sql

---
+++ GCG_4447_BomLink_Dir.sql
@@ -0,0 +1,6 @@
+CREATE TABLE "GCG_4447_BomLink_Dir"(
+ "MFG_Source_Dir" LONGVARCHAR,
+ "MFG_Link_Dir" LONGVARCHAR,
+ "PUR_Source_Dir" LONGVARCHAR,
+ "PUR_Link_Dir" LONGVARCHAR,
+ "Doc_Group" INTEGER DEFAULT '-1' );
